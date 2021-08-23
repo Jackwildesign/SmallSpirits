7ae2b4c2521f796f969bbba271e390c26c14d366
Jackwildesign <57155140+Jackwildesign@users.noreply.github.com>
2021-08-23 21:04:55 +0100

    My chicken walk to mushroom
    Noice.

--- Small_Spirits/Assets/Scripts/ChickenAI.cs
+++ Small_Spirits/Assets/Scripts/ChickenAI.cs
@@ -1,18 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ChickenAI : MonoBehaviour
 {
+    [SerializeField] int hunger = 20;
+    [SerializeField] int health = 5;
+    NavMeshAgent navMeshAgent;
+    bool hungry;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(BecomeHungry());
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (hunger <= 5)
+        {
+            health = health - 1;
+        }
+        else if (hunger <= 15)
+        {
+            HuntForFood();
+            hungry = true;
+        }
+        else if (hunger > 15)
+        {
+            hungry = false;
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    IEnumerator BecomeHungry()
+    {
+        while (gameObject.activeSelf)
+        {
+            hunger = hunger - 1;
+            yield return new WaitForSeconds(5);
+        }
+    }
+
+    private void HuntForFood()
+    {
+        {
+            Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 20);
+            foreach (Collider hitcollider in hitColliders)
+            {
+                print("Searching for Mushrooms, found a " + hitcollider.gameObject.name);
+                if (hitcollider.gameObject.tag == "Mushroom")
+                {
+                    print("Found a mushroom");
+                    navMeshAgent.SetDestination(hitcollider.gameObject.transform.position);
+                    //MoveToFoodAndEat();
+                    return;
+                }
+            }
+        }
+    }
+
+    private static void MoveToFoodAndEat()
+    {
         
+        //Go to first game object
+        //If still there eat it again
+        //If not, run search for food again.
+        //Eat the mushroom
+        //Increase hunger int.
     }
 }
---
+++ Small_Spirits/Assets/Scripts/SatesList.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SatesList : MonoBehaviour
+{
+    
+    //Essential states:
+    //Wander state - Walking around within a given area
+    //Hunting state - Searching for food, plant or prey
+    //Die - If eaten or starved or corrupted etc.
+    
+    
+    //Would be nice to have states:
+    //Idle - not wandering but sitting
+    //Sleeping - Resting in a given area
+    //Breeding - Ready to breed, could involve some specific movements etc. or going to a specific location.
+
+}
